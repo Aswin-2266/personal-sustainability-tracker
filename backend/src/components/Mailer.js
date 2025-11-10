f72fd964e70f2171ddc2b8c1b33c7f8e0bfafb4b
Aswin-2266 <aswinmsc123@gmail.com>
2025-11-10 14:13:36 +0530

    Mailer Edit

--- backend/src/components/Mailer.js
+++ backend/src/components/Mailer.js
@@ -8,7 +8,7 @@
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: parseInt(process.env.MAIL_PORT),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
